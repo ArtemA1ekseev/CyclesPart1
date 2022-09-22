fb0428d5683e71741b6394d0f9663c96ffba66b0
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-09-22 14:42:55 +0400

    Домашнее задание - 2. Задания 1 - 3.

---
+++ src/homework2/Tasks.java
@@ -0,0 +1,20 @@
+package homework2;
+
+public class Tasks {
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание - 2. Задание 1.");
+        for (int i = 1904; i < 2096; i = i + 4) {
+            System.out.println(i + " год является високосным.");
+        }
+        System.out.println(" ");
+        System.out.println("Домашнее задание - 2. Задание 2.");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+        System.out.println(" ");
+        System.out.println("Домашнее задание - 2. Задание 3.");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
+    }
+}
